bc85a5899fb48d417e9520be796b0f63b5bfca8d
Mark Birger <birgerm@yandex.ru>
2024-09-17 12:56:49 +0200

    k8s manifest

--- app.py
+++ app.py
@@ -1,14 +1,42 @@
 from flask import Flask
 import random
+import os
+import time
+
+# load features
+FEATURES = []
+for key, value in os.environ.items():
+    if key.startswith('FEATURE_'):
+        try:
+            failure_rate = int(value)
+            feature = {
+                'name': key,
+                'enabled': True,
+                'failure_rate': failure_rate
+            }
+            FEATURES.append(feature)
+        except ValueError:
+            print(f"Warning: Invalid failure rate for {key}. Skipping this feature.")
+
+print("Enabled features:")
+for feature in FEATURES:
+    if feature['enabled']:
+        print(f"- {feature['name']}")
+        
+start_time = time.time()
 
 app = Flask(__name__)
 
 @app.route('/')
-def random_response():
-    if random.choice([True, False]):
-        return "OK", 200
-    else:
-        return "Internal Server Error", 500
+def feature_response():
+    current_time = time.time()
+    ticks = int(current_time - start_time)
+    
+    for feature in [f for f in FEATURES if f['enabled']]:
+        for it in range(ticks):
+            if random.random() < (ticks / feature['failure_rate'] ):
+                return f"Internal Server Error: {feature['name']} failed at #{it} tick", 500
+    return "OK", 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=7900, debug=True)
